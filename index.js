3a973d504a2be1b86d22f3f15a6d14b53d6e4cbc
tsinaaa <tsinukaldeboch5434@gmail.com>
2025-01-07 22:36:54 +0300

    included local storage and json manifest

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ let myLead = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-console.log(ulEl)
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"))
+if(leadsFromLocalStorage) {
+    myLead = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLead.push(inputEl.value)
-    console.log(myLead)
+    inputEl.value = ""
+    localStorage.setItem("myLead", JSON.stringify(myLead))
+    renderLeads()
 })
+
+function renderLeads() {
+    let leadItems = ""
+    for(let i = 0; i < myLead.length; i++) {
+        leadItems += `
+        <li>
+            <a href="${myLead[i]}" target="_blank">
+                ${myLead[i]}
+            <a/>
+        </li>
+        `
+    }
+    ulEl.innerHTML = leadItems
+}
